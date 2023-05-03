fbf5e74aac8632340b89fdc11141a7d075d08d22
Blankbee <shen.esad@outlook.com>
2023-05-03 23:10:08 +0300

    created detail page

--- market/urls.py
+++ market/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index,contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index, name="index"),
-    path('contact/',contact, name="contact")
+    path('contact/',contact, name="contact"),
+    path('items/',include('item.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
