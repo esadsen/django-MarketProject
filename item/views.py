28787b14b8a12cae73860d3b3f197af0196dbc6e
Blankbee <shen.esad@outlook.com>
2023-05-13 14:38:21 +0300

    item form

--- item/views.py
+++ item/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Item
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def detail(request, pk):
@@ -8,4 +9,11 @@ def detail(request, pk):
     return render(request, 'item/detail.html',{
         'item':item,
         'related_items':related_items
+    })
+@login_required
+def new(request):
+    form=NewItemForm()
+    
+    return(render(request, 'item/form.html'),{
+        'form':form
     })
