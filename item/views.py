8f851ffeb6c5f7fc1ab5a312c69c36cd5bd385dd
Blankbee <shen.esad@outlook.com>
2023-05-21 13:57:26 +0300

    started to search bar

--- item/views.py
+++ item/views.py
@@ -4,6 +4,14 @@ from django.contrib.auth.decorators import login_required
 from .forms import NewItemForm,EditItemForm
 # Create your views here.
 
+
+def items(request):
+    items=Item.objects.filter(is_sold=False)
+    
+    return render(request, 'item/items.html',{
+        'items':items,
+    })
+
 def detail(request, pk):
     item=get_object_or_404(Item,pk=pk)
     related_items=Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
